41990734b5607d8e840da5869b2184eb8055053b
Rohen Agarwal <rohenagarwal@rivian.com>
2025-03-23 11:14:53 -0500

    Working for PWA

---
+++ static/service-worker.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = "3d-cad-viewer-v1";
+const urlsToCache = ["/", "/static/style.css", "/static/upload.js", "/static/manifest.json"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
